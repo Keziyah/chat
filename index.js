583c6840b34d28571729f69db48533649241d2e8
Keziyah <keziyahlewis@gmail.com>
2017-06-06 22:10:54 -0400

    file structure, dependencies, webpack config

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(3000, function() {
+    console.log("View Keziyah's project at localhost:3000.")
+})
